01d9b746274b2dcb909fb4818872fcf9b75169ec
Luciano Silva <legostr@gmail.com>
2025-01-26 20:15:24 -0300

    refactor: restructure application components and remove unused App.js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
+
+export default function App(props) {
+  return (
+    <React.StrictMode>
+      <h1>Fundamentos React</h1>
+      <Fragmento />
+      <ComParametro titulo="Situação do aluno" aluno="Pedro Silva" nota={9.3} />
+      <Primeiro />
+    </React.StrictMode>
+  );
+}
