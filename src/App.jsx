18f2a4020d55065e5123df6f3813e7732184a2a5
Luciano Silva <legostr@gmail.com>
2025-02-06 11:42:29 -0300

    feat: add Aleatorio component and update Primeiro message

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Aleatorio from "./components/basicos/Aleatorio";
 
 export default function App(props) {
   return (
     <React.StrictMode>
       <h1>Fundamentos React</h1>
+      <Aleatorio min={10} max={60} />
       <Fragmento />
       <ComParametro titulo="Situação do aluno" aluno="Pedro Silva" nota={9.3} />
       <Primeiro />
